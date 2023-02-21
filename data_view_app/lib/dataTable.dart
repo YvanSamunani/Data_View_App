b2bfa8783bc1a99f15e2cdf91d593a07764d7e1d
YvanSamunani <90831373+YvanSamunani@users.noreply.github.com>
2023-02-21 21:02:40 +0200

    Update dataTable.dart

--- data_view_app/lib/dataTable.dart
+++ data_view_app/lib/dataTable.dart
@@ -1,32 +1,35 @@
 import "package:flutter/material.dart";
 void main() => runApp(Data_Table());
-class Data_Table extends StatelessWidget {
- Widget build(BuildContext inContext) {
- return MaterialApp(home : Scaffold(
- body : Column(children : [
- Container(height : 100),
- DataTable(sortColumnIndex : 1,
- columns : [
- DataColumn(label : Text("First Name")),
- DataColumn(label : Text("Last Name"))
- ],
- rows : [
- DataRow(cells : [
- DataCell(Text("Leia")),
- DataCell(Text("Organa"), showEditIcon : true)
- ]),
- DataRow(cells : [
- DataCell(Text("Luke")),
- DataCell(Text("Skywalker"))
- ]),
 
- DataRow(cells : [
- DataCell(Text("Han")),
- DataCell(Text("Solo"))
- ])
- ]
- )
- ])
- ));
- }
+class Data_Table extends StatelessWidget {
+  Widget build(BuildContext inContext) {
+    return MaterialApp(
+      home : Scaffold(
+        body : Column(
+          children : [
+            Container(height : 100),
+            DataTable(
+              sortColumnIndex : 1,
+              columns : [
+                DataColumn(label : Text("First Name")),
+                DataColumn(label : Text("Last Name"))
+              ],
+              rows : [
+                DataRow(cells : [
+                  DataCell(Text("Leia")),
+                  DataCell(Text("Organa"), showEditIcon : true)
+                  ]),
+                DataRow(cells : [
+                  DataCell(Text("Luke")),
+                  DataCell(Text("Skywalker"))
+                ]),
+
+                DataRow(cells : [
+                  DataCell(Text("Han")),
+                  DataCell(Text("Solo"))
+                ])
+            ])
+        ])
+      ));
+  }
 }
